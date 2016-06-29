eaf05613278070a9ebfc352d3d9aebd2cedf1bce
Michael Berber <mickberber@icloud.com>
2016-06-29 11:49:32 -0700

    init mario ex, complete recursive checker

---
+++ c-ercises/mario.c
@@ -0,0 +1,37 @@
+/*
+Itsa Mario
+Toward the end of World 1-1 in Nintendo’s Super Mario Brothers, Mario must ascend a "half-pyramid" of blocks before leaping (if he wants to maximize his score) toward a flag pole. 
+
+Write, in a file called mario.c in your ~/workspace/pset1 directory, a program that recreates this half-pyramid using hashes (#) for blocks. However, to make things more interesting, first prompt the user for the half-pyramid’s height, a non-negative integer no greater than 23. (The height of the half-pyramid pictured above happens to be 8.) If the user fails to provide a non-negative integer no greater than 23, you should re-prompt for the same again. Then, generate (with the help of printf and one or more loops) the desired half-pyramid. Take care to align the bottom-left corner of your half-pyramid with the left-hand edge of your terminal window, as in the sample output below, wherein underlined text represents some user’s input.
+
+*/
+#include <stdio.h>
+
+int checker(int i) 
+{
+    if(i >= 0 && i <= 23) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+int getinput(void) 
+{
+    int arr[BUFSIZ];
+    printf("Height:\n");
+    scanf("%d", arr);
+    int newnum = arr[0];
+    if(checker(newnum)) {
+        return newnum;
+    }
+    return getinput();
+}
+
+int main(void) 
+{
+    int num = getinput();
+
+    printf("%d\n", num);
+}
